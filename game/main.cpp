6b4732c278b3795aef862e8047596d5161230e56
Randall Hines <randallhines0@gmail.com>
2019-06-04 20:38:10 -0700

    -lines, sprites, and boxes

--- game/main.cpp
+++ game/main.cpp
@@ -3,11 +3,23 @@
 
 #define SDL_MAIN_HANDLED
 #include "SDL.h"
+#include "entt/entt.hpp"
 #include <cstdlib>
 #include <iostream>
 
-int main()
-{
+char WINDOW_NAME[] = { "Ayse please stop this." };
+
+int SCREEN_WIDTH = 1280;
+int SCREEN_HEIGHT = 720;
+
+bool quit = false;
+
+SDL_Event e;
+
+SDL_Window* win;
+SDL_Renderer* ren;
+
+int init() {
 	using std::cout;
 	using std::endl;
 
@@ -16,16 +28,22 @@ int main()
 		return EXIT_FAILURE;
 	}
 
-	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 620, 387, SDL_WINDOW_SHOWN);
+	win = SDL_CreateWindow(WINDOW_NAME, 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (win == nullptr) {
 		cout << "SDL_CreateWindow Error: " << SDL_GetError() << endl;
 		return EXIT_FAILURE;
 	}
 
-	SDL_Renderer* ren
-		= SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren == nullptr) {
 		cout << "SDL_CreateRenderer Error" << SDL_GetError() << endl;
+		return EXIT_FAILURE;
+	}
+}
+
+int main()
+{
+	if (init() == EXIT_FAILURE) {
 		return EXIT_FAILURE;
 	}
 
@@ -57,23 +75,59 @@ int main()
 
 	SDL_Surface* bmp = SDL_LoadBMP("assets/grumpy-cat.bmp");
 	if (bmp == nullptr) {
-		cout << "SDL_LoadBMP Error: " << SDL_GetError() << endl;
+		SDL_Log("SDL_LoadBMP Error: %s", SDL_GetError());
 		return EXIT_FAILURE;
 	}
 
 	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, ayse_surf);
 	SDL_FreeSurface(ayse_surf);
 	if (tex == nullptr) {
-		cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << endl;
+		SDL_Log("SDL_CreateTextureFromSurface Error: %s", SDL_GetError());
 		return EXIT_FAILURE;
 	}
 
-	for (int i = 0; i < 20; i++) {
+	SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4,SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+	SDL_Rect boxRect = fillRect;
+	boxRect.w = boxRect.w / 2;
+	boxRect.h = boxRect.h / 2;
+	boxRect.x = 0;
+
+
+	while (!quit) {
+
+		while (SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_QUIT) {
+				quit = true;
+			}
+
+			if (e.key.keysym.sym == SDLK_ESCAPE) {
+				quit = true;
+			}
+		}
+
+		SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0xFF);
 		SDL_RenderClear(ren);
-		SDL_RenderCopy(ren, tex, nullptr, nullptr);
+		//Draw a line renderer, texture, portion of the tex, portion of the destRect
+		SDL_RenderCopy(ren, tex, nullptr, &fillRect);
+
+		//Draw a box
+		SDL_SetRenderDrawColor(ren, 0xFF, 0x00, 0x00, 0xFF);
+		SDL_RenderFillRect(ren, &boxRect);
+
+		//Draw a line
+		SDL_SetRenderDrawColor(ren,0, 0, 255, 255);
+		SDL_RenderDrawLine(ren, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+
+		//Draw a dot
+		SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0x00, 0xFF);
+		for (int i = 0; i < SCREEN_HEIGHT; i += 4) {
+			SDL_RenderDrawPoint(ren, SCREEN_WIDTH / 2, i);
+		}
+
 		SDL_RenderPresent(ren);
-		SDL_Delay(100);
 	}
+		
 
 	stbi_image_free(ayse);
 	SDL_DestroyTexture(tex);
