825c0c520393c4525ab205b8eef1d380bd044b18
Randall Hines <randallhines0@gmail.com>
2019-09-22 09:25:05 -0700

    -madness

--- updatesubs.bat
+++ updatesubs.bat
@@ -1,11 +1,2 @@
-mkdir thirdparty
-git submodule init
-git submodule update
-cd thirdparty/bgfx.cmake
-git submodule init
-git submodule update
-cd ../stb-cmake
-git submodule init
-git submodule update
-cd ../glm
+cd thirdparty/glm
 git checkout 0.9.9.5
