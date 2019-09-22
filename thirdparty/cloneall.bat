94e8af5da4ecaeabbcda3ba3d41717ea751ef994
Randall Hines <randallhines0@gmail.com>
2019-09-22 09:04:33 -0700

    -remove uneeded bat

--- thirdparty/cloneall.bat
+++
@@ -1,8 +0,0 @@
-git submodule add -f https://github.com/JoshuaBrookover/bgfx.cmake
-git submodule add -f https://github.com/g-truc/glm
-git submodule add -f https://github.com/spurious/SDL-mirror
-git submodule add -f https://github.com/madler/zlib
-git submodule add -f https://github.com/skypjack/entt
-git submodule add -f https://github.com/nothings/stb
-git submodule init
-git submodule update
