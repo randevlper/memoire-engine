36f01ca7ad397b98fe1a6ad53f01d1a15101eaf2
randevlper <randevlper@gmail.com>
2021-03-31 18:57:23 -0700

    -Update BGFX

--- generate_cmake.bat
+++ generate_cmake.bat
@@ -3,10 +3,3 @@ Pushd build
 cmake ../ %PAREM%
 Pushd ..
 pause
-if not exist "build\memoire-enginee.sln" exit
-for /f "usebackq tokens=1* delims=: " %%i in (`vswhere.exe -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop`) do (
-	if /i "%%i"=="installationPath" (
-	"%%j\Common7\IDE\devenv.exe" "build\memoire-engine.sln"
-	exit
-	)
-)
